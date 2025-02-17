a676fc944c5c6d2621828ba9e3a475e15c81e8fd
ongaeshi <ongaeshi0621@gmail.com>
2025-02-18 01:33:24 +0900

    feat: sayagata

--- main.rb
+++ main.rb
@@ -1,17 +1,64 @@
-# https://gainenz.com/2021/05/spirograph/
-
 def setup
-  createCanvas(640, 480)
-  frameRate(20)
-  background(255)
+  createCanvas(400, 400)
+  colorMode(HSB, 360, 100, 100)
 end
 
-@r = 10
-@theta = 0
+def draw_sayagata_pattern
+  background(255)
+  strokeWeight(2)
+
+  size = 40
+  rows = height / size
+  cols = width / size
+
+  rows.to_i.times do |i|
+    cols.to_i.times do |j|
+      x = j * size
+      y = i * size
+
+      hue = (i * 30 + j * 30) % 360
+      fill(hue, 50, 100, 0.5) # Add fill color with some transparency
+      stroke(hue, 50, 100)
+      sayagata(x, y, size)
+    end
+  end
+end
+
+def sayagata(x, y, size)
+  half = size / 2
+  quarter = size / 4
+
+  # Draw the Sayagata pattern
+  beginShape
+  vertex(x + quarter, y)
+  vertex(x + quarter, y + half)
+  vertex(x, y + half)
+  vertex(x, y + size)
+  vertex(x + half, y + size)
+  vertex(x + half, y + half + quarter)
+  vertex(x + size, y + half + quarter)
+  vertex(x + size, y)
+  vertex(x + half + quarter, y)
+  vertex(x + half + quarter, y + half)
+  vertex(x + size, y + half)
+  vertex(x + size, y + size)
+  vertex(x + half, y + size)
+  vertex(x + half, y + half + quarter)
+  vertex(x, y + half + quarter)
+  vertex(x, y)
+  vertex(x + half, y)
+  vertex(x + half, y + quarter)
+  vertex(x + size, y + quarter)
+  vertex(x + size, y + half)
+  vertex(x + half + quarter, y + half)
+  vertex(x + half + quarter, y + size)
+  vertex(x, y + size)
+  vertex(x, y + half + quarter)
+  vertex(x + quarter, y + half + quarter)
+  vertex(x + quarter, y)
+  endShape(CLOSE)
+end
 
 def draw
-  @theta += 10
-  @r += 1
-  line(@r * cos(radians(@theta)) + width / 2, @r * sin(radians(@theta)) + height / 2,
-       @r * cos(radians(@theta) * 1.05) + width / 2, @r * sin(radians(@theta) * 1.05) + height / 2)
+  draw_sayagata_pattern
 end
